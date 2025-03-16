541ab66aaab79df7e36ef38d2874ec8f1ea0c9d4
voltaire-banyo <voltairebanyo350@gmail.com>
2025-03-16 13:02:16 +0100

    my third essay
    this is my python file

---
+++ detail.py
@@ -0,0 +1,9 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name=" mbailassem voltaire "
+>>> age="98"
+>>> school="Ahmadu Bello University"
+>>> trainning="phyton programm"
+>>> association="COESA"
+>>> print(" my name is "+name+" and i am "+age+" i school in "+school+" i am attending "+trainning+" organise by "+association)
+ my name is  mbailassem voltaire  and i am 98 i school in Ahmadu Bello University i am attending phyton programm organise by COESA
